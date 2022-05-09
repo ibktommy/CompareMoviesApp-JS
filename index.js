09fb2d767fcc32e07631b57800661f0df90354e6
ibktommy <atomquartz@gmail.com>
2022-05-09 15:29:08 +0100

    Creating Dynamic HTML elements in JS file and appending it to the DOM.

--- index.js
+++ index.js
@@ -14,21 +14,42 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
+// Creating Dynamic HTML Elements Thats Displays The Fetched Data
+const dataContainer = document.querySelector(".data-container");
+dataContainer.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" type='search'>
+    <div class='dropdown'>
+      <div class= 'dropdown-menu'>
+        <div class='dropdown-content results'></div>
+      </div>
+    </div>
+  `;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
 //Selecting Input Field for Search Results that Fetches Movie Data
 const searchInput = document.querySelector("input");
 
 const onInput = async (e) => {
 	const movies = await fetchData(e.target.value);
-	console.log(movies);
 
+	// Add class to display Menu Dropdown
+	dropdown.classList.add("is-active");
+
+	// Create Loop to run through Data Result
 	for (let eachMovie of movies) {
-		const div = document.createElement("div");
-		div.innerHTML = `
-                      <img src="${eachMovie.Poster}"/>
-                      <h1>${eachMovie.Title}</h1>
-                    `;
-		document.querySelector("#target").appendChild(div);
+		const option = document.createElement("a");
+
+		option.classList.add("dropdown-item");
+		option.innerHTML = `
+      <img src="${eachMovie.Poster}"/>
+      ${eachMovie.Title}
+    `;
+		resultsWrapper.appendChild(option);
 	}
 };
 
-searchInput.addEventListener("input", debounce(onInput, 800));
+searchInput.addEventListener("input", debounce(onInput, 1000));
